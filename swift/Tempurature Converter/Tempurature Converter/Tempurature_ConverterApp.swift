efeb42b040936a27733ac392d50bd44ddc0dca35
Warren Su <warrensuca@gmail.com>
2025-06-21 16:38:21 -0700

    inital commit

---
+++ swift/Tempurature Converter/Tempurature Converter/Tempurature_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tempurature_ConverterApp.swift
+//  Tempurature Converter
+//
+//  Created by warren su on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Tempurature_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
